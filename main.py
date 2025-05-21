e1df3702a2a68d91692921ebce6f919ee8ceeeeb
Pavel Gorbachev <pavelgorbachev@MacBook-Air-Pavel.local>
2025-05-21 03:40:14 +0300

    fix

--- main.py
+++ main.py
@@ -3,13 +3,11 @@ import json
 import os
 from time import sleep
 
-import psutil
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 while True:
-    load1, load5, load15 = psutil.getloadavg()
-    ram_usage_percent = psutil.virtual_memory().percent
+    load1, load5, load15 = os.getloadavg()
 
     log = {
         'load_avg': {
@@ -17,11 +15,10 @@ while True:
             'load5': load5,
             'load15': load15,
         },
-        'ram_usage_percent': ram_usage_percent,
         'asctime': datetime.datetime.utcnow().isoformat(timespec='milliseconds', sep=' ')
     }
 
-    with open(os.path.join(BASE_DIR, 'host_metrics_app.log'), 'a') as f:
+    with open(os.path.join(BASE_DIR, './host_metrics_app/host_metrics_app.log'), 'a') as f:
         f.write(json.dumps(log) + '\n')
 
     sleep(7)
